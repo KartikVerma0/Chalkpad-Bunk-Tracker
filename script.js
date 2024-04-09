c9251fe7dc3ed145835ed545d914ad0f54d09a7a
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-04-09 15:09:55 +0530

    Improved functionality to show multiple options for different target attendance

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const allSubjectsDetailContainer = document.querySelector("#attendanceGraph").ch
 
 const subjectsArray = [];
 
-for(let subject = 2;subject<allSubjectsDetailContainer.length;){
+for (let subject = 2; subject < allSubjectsDetailContainer.length;) {
 	subjectsArray.push(allSubjectsDetailContainer[subject]);
-	subject+=2;
+	subject += 2;
 }
 
 const deliveredDetails = {};
@@ -13,86 +13,107 @@ const medicalLeaveDetails = {};
 const dutyLeaveDetails = {};
 
 
-for(let i = 0;i<subjectsArray.length;i++){
+for (let i = 0; i < subjectsArray.length; i++) {
 	let delivered = parseInt(subjectsArray[i].children[2].children[0].children[1].children[0].innerText);
 	deliveredDetails[`${i}`] = delivered;
 	let attended = parseInt(subjectsArray[i].children[2].children[0].children[2].children[0].innerText);
 	attendedDetails[`${i}`] = attended;
-	let medicalLeave = parseInt(subjectsArray[i].children[2].children[0].children[3].children[0].innerText); 
-        medicalLeaveDetails[`${i}`] = medicalLeave;
+	let medicalLeave = parseInt(subjectsArray[i].children[2].children[0].children[3].children[0].innerText);
+	medicalLeaveDetails[`${i}`] = medicalLeave;
 	let dutyLeave = parseInt(subjectsArray[i].children[2].children[0].children[4].children[0].innerText);
-        dutyLeaveDetails[`${i}`] = dutyLeave;
+	dutyLeaveDetails[`${i}`] = dutyLeave;
 }
 
 const totalAttandance = {};
 
-for(let i = 0; i<subjectsArray.length; i++){
+for (let i = 0; i < subjectsArray.length; i++) {
 	totalAttandance[`${i}`] = attendedDetails[i] + medicalLeaveDetails[i] + dutyLeaveDetails[i];
 }
 
-// add option to change target in future
-let targetPercentage = 75;
-
-for(let i = 0; i<subjectsArray.length; i++){
-	let isAboveTarget = false;
-	let attPercentage = (totalAttandance[i]/deliveredDetails[i])*100;
-	if(attPercentage >= targetPercentage){
-		isAboveTarget = true;
-	}
-	let rightDivList = subjectsArray[i].children[2].children[0];
-	
-	if(isAboveTarget){
-		let bunkClassNumber = 0;
-		let attPerCopy = attPercentage;
-		let attendedCopy = totalAttandance[i];
-		let deliveredCopy = deliveredDetails[i];
-		while(attPerCopy >= targetPercentage){
-			bunkClassNumber++;
-			deliveredCopy++;
-			attPerCopy = (attendedCopy/deliveredCopy)*100;
+var toggleRowBg = true;
+function findBunkOrToAttend(targetPercentage, toggleRowBg) {
+	for (let i = 0; i < subjectsArray.length; i++) {
+		let isAboveTarget = false;
+		let attPercentage = (totalAttandance[i] / deliveredDetails[i]) * 100;
+		if (attPercentage >= targetPercentage) {
+			isAboveTarget = true;
 		}
+		let rightDivList = subjectsArray[i].children[2].children[0];
 
-		//when the attandance is above target the loop runs one extra time
-		//causing the bunkClass count and delivered to increase by one
-		//resulting in attandance getting lower than target
-		//so we reduce bunkClass count by 1
-		//and delivered class count by 1
-		//and recalculate attandance percentage
-		bunkClassNumber--;
-		deliveredCopy--;
-		attPerCopy = (attendedCopy/deliveredCopy)*100;
+		if (isAboveTarget) {
+			let bunkClassNumber = 0;
+			let attPerCopy = attPercentage;
+			let attendedCopy = totalAttandance[i];
+			let deliveredCopy = deliveredDetails[i];
+			while (attPerCopy >= targetPercentage) {
+				bunkClassNumber++;
+				deliveredCopy++;
+				attPerCopy = (attendedCopy / deliveredCopy) * 100;
+			}
 
-		let newListItem = document.createElement('li');
-		newListItem.style.fontSize='12px';
-		newListItem.classList.add('trow1');
-		newListItem.innerText = `Maximum classes you can bunk: (Target Attandance- ${targetPercentage}%) : `;
-		let newSpan = document.createElement('span');
-		newSpan.style.color='limegreen';
-		newSpan.innerText = `${bunkClassNumber}`;
-		newListItem.append(newSpan);
-		rightDivList.appendChild(newListItem);
-	}else{
-		let classesToAttendNumber = 0;
-		let attPerCopy = attPercentage;
-                let attendedCopy = totalAttandance[i];
-                let deliveredCopy = deliveredDetails[i];
-                while(attPerCopy < targetPercentage){
-			attendedCopy++;
-                        classesToAttendNumber++;
-                        deliveredCopy++;
-                        attPerCopy = (attendedCopy/deliveredCopy)*100;
-                }
+			//when the attandance is above target the loop runs one extra time
+			//causing the bunkClass count and delivered to increase by one
+			//resulting in attandance getting lower than target
+			//so we reduce bunkClass count by 1
+			//and delivered class count by 1
+			//and recalculate attandance percentage
+			bunkClassNumber--;
+			deliveredCopy--;
+			attPerCopy = (attendedCopy / deliveredCopy) * 100;
 
-		let newListItem = document.createElement('li');
-		newListItem.style.fontSize='12px'; 
-                newListItem.classList.add('trow1');
-                newListItem.innerText = `Number of classes to reach ${targetPercentage}%: `;
-		let newSpan = document.createElement('span');
-		newSpan.style.color='red';
-		newSpan.innerText = `${classesToAttendNumber}`;
-		newListItem.append(newSpan);
-		rightDivList.appendChild(newListItem);
+			let newListItem = document.createElement('li');
+			newListItem.style.fontSize = '12px';
+			if (toggleRowBg) {
+				newListItem.classList.add('trow1');
+
+			} else {
+				newListItem.classList.add('trow0');
+
+			}
+			newListItem.innerText = `Maximum classes you can bunk: (Target Attandance- ${targetPercentage}%) : `;
+			let newSpan = document.createElement('span');
+			newSpan.style.color = 'limegreen';
+			newSpan.innerText = `${bunkClassNumber}`;
+			newListItem.append(newSpan);
+			rightDivList.appendChild(newListItem);
+		} else {
+			let classesToAttendNumber = 0;
+			let attPerCopy = attPercentage;
+			let attendedCopy = totalAttandance[i];
+			let deliveredCopy = deliveredDetails[i];
+			while (attPerCopy < targetPercentage) {
+				attendedCopy++;
+				classesToAttendNumber++;
+				deliveredCopy++;
+				attPerCopy = (attendedCopy / deliveredCopy) * 100;
+			}
+
+			let newListItem = document.createElement('li');
+			newListItem.style.fontSize = '12px';
+			if (toggleRowBg) {
+				newListItem.classList.add('trow1');
+
+			} else {
+				newListItem.classList.add('trow0');
+
+			}
+			newListItem.innerText = `Number of classes to reach ${targetPercentage}%: `;
+			let newSpan = document.createElement('span');
+			newSpan.style.color = 'red';
+			newSpan.innerText = `${classesToAttendNumber}`;
+			newListItem.append(newSpan);
+			rightDivList.appendChild(newListItem);
+		}
 	}
-
-	//console.log(isAboveTarget);
 }
+
+findBunkOrToAttend(90, toggleRowBg);
+toggleRowBg = !toggleRowBg;
+findBunkOrToAttend(75, toggleRowBg);
+toggleRowBg = !toggleRowBg;
+findBunkOrToAttend(65, toggleRowBg);
+toggleRowBg = !toggleRowBg;
+findBunkOrToAttend(50, toggleRowBg);
+
+
+
